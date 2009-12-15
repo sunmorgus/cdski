93efda412babf0be476ed12065f8349fb123e793
(no author) <(no author)@a6ec7c5c-bb92-4c44-a628-85f73da2c26c>
2009-12-15 02:38:53 +0000

    creating acceleration control branch
    git-svn-id: file:///media/house/svn/skipre@17 a6ec7c5c-bb92-4c44-a628-85f73da2c26c

---
+++ branches/acceleration/trunk/app/assistants/highscores-assistant.js
@@ -0,0 +1,171 @@
+function HighscoresAssistant(params){
+    this.hsDB = params.db;
+    if (params.score) {
+        this.Score = params.score;
+    }
+    if (params.skier) {
+        this.chosenSkier = params.skier;
+    }
+}
+
+HighscoresAssistant.prototype.setup = function(){
+    this.appMenuModel = {
+        visible: true,
+        items: [Mojo.Menu.editItem, {
+            label: $L('Clear High Scores'),
+            command: 'clearScores'
+        }]
+    };
+    
+    this.controller.setupWidget(Mojo.Menu.appMenu, {
+        omitDefaultItems: true
+    }, this.appMenuModel);
+    
+    this.innerListAttrs = {
+        listTemplate: 'highscores/listcontainer',
+        itemTemplate: 'highscores/listItem'
+    };
+    this.resultList = [{
+        resultString: " "
+    }]
+    this.listModel = {
+        items: this.resultList
+    };
+    this.controller.setupWidget('results_list', this.innerListAttrs, this.listModel);
+    Mojo.Event.listen($('retry'), Mojo.Event.tap, this.retry.bind(this));
+    Mojo.Event.listen($('quit'), Mojo.Event.tap, this.quit.bind(this));
+}
+
+HighscoresAssistant.prototype.handleCommand = function(event){
+    this.controller = Mojo.Controller.stageController.activeScene();
+    if (event.type == Mojo.Event.command) {
+        switch (event.command) {
+            case 'clearScores':
+                this.dropTable();
+                this.resultList.clear();
+                this.controller.modelChanged(this.listModel, this);
+                break;
+        }
+    }
+}
+
+HighscoresAssistant.prototype.activate = function(event){
+    this.getHighScores();
+    if (!this.Score) {
+        $('retry').style.display = 'none';
+        $('quit').innerHTML = 'Back';
+    }
+    else {
+        this.checkScore(this.Score);
+    }
+}
+
+HighscoresAssistant.prototype.callback = function(value){
+    if (this.Score) {
+        this.addHighScore(value);
+    }
+}
+
+HighscoresAssistant.prototype.deactivate = function(event){
+}
+
+HighscoresAssistant.prototype.cleanup = function(event){
+}
+
+HighscoresAssistant.prototype.getHighScores = function(){
+    var query = 'select name, score from highScore order by score desc;'
+    this.hsDB.transaction((function(transaction){
+        transaction.executeSql(query, [], this.buildList.bind(this), this.errorHandler.bind(this));
+    }).bind(this));
+}
+
+HighscoresAssistant.prototype.checkScore = function(score){
+    var query = 'select score from highScore where ' + score + ' > (select score from highScore order by score asc limit 1);';
+    this.hsDB.transaction((function(transaction){
+        transaction.executeSql(query, [], this.isHighScore.bind(this), this.errorHandler.bind(this));
+    }).bind(this));
+}
+
+HighscoresAssistant.prototype.addHighScore = function(name){
+    var query = 'INSERT INTO highScore (id, name, score) VALUES (?,?,?); GO;'
+    this.hsDB.transaction((function(transaction){
+        transaction.executeSql(query, [Math.random(), name, this.Score], this.getHighScores.bind(this), this.errorHandler.bind(this));
+    }).bind(this));
+}
+
+HighscoresAssistant.prototype.dropTable = function(){
+    var query = 'DELETE FROM highScore; GO;';
+    this.hsDB.transaction((function(transaction){
+        transaction.executeSql(query, []);
+    }).bind(this));
+}
+
+HighscoresAssistant.prototype.buildList = function(transaction, results){
+    // Handle the results 
+    var string = "";
+    
+    try {
+        var list = [];
+        for (var i = 0; i < results.rows.length; i++) {
+            var row = results.rows.item(i);
+            
+            var rowClass = "";
+            
+            if (i === 0) {
+                var rowClass = "first";
+            }
+            else 
+                if (i === results.rows.length - 1) {
+                    var rowClass = "last";
+                }
+            
+            var string = {
+                score: row["score"],
+                name: row["name"],
+                rowClass: rowClass
+            };
+            
+            list.push(string);
+        }
+        //update the list widget
+        this.resultList.clear();
+        Object.extend(this.resultList, list);
+        this.controller.modelChanged(this.listModel, this);
+    } 
+    catch (e) {
+        $('result').update(e);
+    }
+}
+
+HighscoresAssistant.prototype.isHighScore = function(transaction, results){
+    if (results.rows.length > 0 || this.resultList.length == 0) {
+        this.controller.showDialog({
+            template: 'entry/entry-scene',
+            assistant: new EntryAssistant(this, this.callback.bind(this), this.Score),
+            preventCancel: true
+        });
+    }
+}
+
+HighscoresAssistant.prototype.retry = function(event){
+    var params = {
+        chosen: this.chosenSkier,
+        db: this.hsDB
+    }
+    
+    this.controller.stageController.assistant.showScene("game", 'game', params);
+}
+
+HighscoresAssistant.prototype.quit = function(event){
+    var params = {
+        chosen: this.chosenSkier,
+		db: this.hsDB
+    }
+    
+    this.controller.stageController.assistant.showScene("start", 'start', params);
+}
+
+HighscoresAssistant.prototype.errorHandler = function(transaction, error){
+    console.log('Error was ' + error.message + ' (Code ' + error.code + ')');
+    return true;
+}
---
+++ branches/acceleration/trunk/app/assistants/start-assistant.js
@@ -0,0 +1,91 @@
+function StartAssistant(params){
+    if (params) {
+        this.hsDB = params.db;
+		this.chosenSkier = params.chosen
+    }
+}
+
+StartAssistant.prototype.hsDB = null;
+StartAssistant.prototype.chosenSkier = null;
+StartAssistant.prototype.skier = null;
+
+StartAssistant.prototype.setup = function(){
+    this.controller.setupWidget('riley', {}, {
+        buttonLabel: 'Skier'
+    });
+	this.riley = this.skierRiley.bind(this);
+	Mojo.Event.listen($('riley'), Mojo.Event.tap, this.riley);
+	
+    this.controller.setupWidget('aiden', {}, {
+        buttonLabel: '\'Boarder'
+    });
+	this.aiden = this.skierAiden.bind(this);
+	Mojo.Event.listen($('aiden'), Mojo.Event.tap, this.aiden);
+	
+    this.start = this.startGame.bind(this);
+    Mojo.Event.listen($('startGame'), Mojo.Event.tap, this.start);
+    
+    this.highscores = this.highScores.bind(this);
+    Mojo.Event.listen($('highScores'), Mojo.Event.tap, this.highscores);
+    
+    this.createDB();
+	
+	this.chosenSkier = 'riley';
+}
+
+StartAssistant.prototype.activate = function(event){
+
+}
+
+StartAssistant.prototype.deactivate = function(event){
+}
+
+StartAssistant.prototype.cleanup = function(event){
+}
+
+StartAssistant.prototype.createDB = function(){
+    try {
+        var name = "SkiPreDB";
+        var version = "0.1";
+        
+        /* Open (or create) the database */
+        this.hsDB = openDatabase(name, version, 'SkiPreDataStore', 100000);
+        
+        if (this.hsDB) {
+            /* Create Table(s) */
+            var string = 'CREATE TABLE IF NOT EXISTS highScore (id TEXT PRIMARY KEY DESC DEFAULT "nothing", name TEXT NOT NULL DEFAULT "nothing", score INTEGER NOT NULL DEFAULT "nothing"); GO;'
+            this.hsDB.transaction((function(transaction){
+                transaction.executeSql(string, [])
+            }).bind(this));
+        }
+    } 
+    catch (e) {
+        console.log('Error: ' + e);
+    }
+}
+
+StartAssistant.prototype.skierRiley = function(){
+	$('skier').src = 'images/sprites/r/riley_down.png';
+}
+
+StartAssistant.prototype.skierAiden = function(){
+	$('skier').src = 'images/sprites/r/riley_left.png';
+}
+
+StartAssistant.prototype.startGame = function(event){
+    var params = {
+        chosen: this.chosenSkier,
+        db: this.hsDB
+    }
+    
+    this.controller.stageController.assistant.showScene("game", 'game', params);
+}
+
+StartAssistant.prototype.highScores = function(event){
+    var params = {
+		chosen: this.chosenSkier,
+        db: this.hsDB
+    }
+    
+    this.controller.stageController.assistant.showScene("highscores", 'highscores', params);
+}
