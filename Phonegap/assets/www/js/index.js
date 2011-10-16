8c0986baf42f07ff6696e3a3ee4305055a1ca68c
sunmorgus <sunmorgus@a6ec7c5c-bb92-4c44-a628-85f73da2c26c>
2011-10-16 14:06:04 +0000

    added fix for page transitions (sort of, they're still kinda ugly); added initial fix for new game;
    git-svn-id: file:///media/house/svn/skipre@102 a6ec7c5c-bb92-4c44-a628-85f73da2c26c

--- Phonegap/assets/www/js/index.js
+++ Phonegap/assets/www/js/index.js
@@ -4,26 +4,40 @@ $(function() {
 var storage = window.localStorage;
 
 $(document).bind("pagebeforechange", function(e, data) {
+	if (typeof data.toPage === "string") {
+		var u = $.mobile.path.parseUrl(data.toPage);
+		switch (u.hash) {
+		case "#game":
+			break;
+		default:
+			_obstacles.splice(0, _obstacles.length);
+			var params = {
+				chosen : "riley"
+			}
 
+			SetupCanvas(params);
+			break;
+		}
+	}
 });
 
 $(document).bind("pagechange", function(e, data) {
-//	alert(typeof data.toPage.selector);
-//	var str="";
-//	for(prop in data.toPage)
-//	{
-//		str += prop + " value: " + data.toPage[prop] + "\n";
-//	}
-//	console.log(str); // Show all properties and its value
+	// alert(typeof data.toPage.selector);
+	// var str="";
+	// for(prop in data.toPage)
+	// {
+	// str += prop + " value: " + data.toPage[prop] + "\n";
+	// }
+	// console.log(str); // Show all properties and its value
 	if (typeof data.toPage.selector === "string") {
-		if(data.toPage.selector.indexOf("game") >= 0){
+		if (data.toPage.selector.indexOf("game") >= 0) {
 			snowStorm.stop();
 			snowStorm.freeze();
-			
+
 			var params = {
-				chosen: "riley"
+				chosen : "riley"
 			}
-			
+
 			SetupCanvas(params);
 		}
 	}
@@ -40,6 +54,8 @@ function onDeviceReady() {
 	// start the snow
 	snowStorm.show();
 	snowStorm.resume();
+
+	$('.ui-page').css('minHeight', screen.availHeight);
 }
 
 // app exit
@@ -49,5 +65,5 @@ function onPause() {
 
 // app open
 function onResume() {
-	//StartMainLoop();
+	// StartMainLoop();
 }
